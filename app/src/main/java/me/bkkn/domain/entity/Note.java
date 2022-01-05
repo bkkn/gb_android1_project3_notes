90add41b19b38906b527c608c1dd01eb488b95fa
bkkn <bakaikin@yandex.ru>
2022-01-05 23:49:19 +0300

    Notes repo interface added

--- app/src/main/java/me/bkkn/domain/entity/Note.java
+++ app/src/main/java/me/bkkn/domain/entity/Note.java
@@ -1,4 +1,4 @@
-package me.bkkn.domain.entities;
+package me.bkkn.domain.entity;
 
 import android.os.Parcel;
 import android.os.Parcelable;
---
+++ app/src/main/java/me/bkkn/domain/repository/Notes.java
@@ -0,0 +1,11 @@
+package me.bkkn.domain.repository;
+
+import java.util.List;
+
+import me.bkkn.domain.entity.Note;
+
+public interface Notes {
+    List<Note>getNotes();
+
+    void deleteNote(Note note);
+}
