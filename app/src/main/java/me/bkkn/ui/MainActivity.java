e9fb089bc219754f6372078321c5fff52ae01bdd
bkkn <bakaikin@yandex.ru>
2022-01-06 00:58:18 +0300

    ui classes and layouts added

--- app/src/main/java/me/bkkn/ui/MainActivity.java
+++ app/src/main/java/me/bkkn/ui/MainActivity.java
@@ -1,16 +1,67 @@
 package me.bkkn.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import me.bkkn.App;
 import me.bkkn.R;
+import me.bkkn.domain.entity.Note;
+import me.bkkn.domain.repository.Notes;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnNoteListener {
+    private static final int NOTE_REQUEST_CODE = 42;
+
+    private Notes notes;
+
+    private RecyclerView recyclerView;
+    private NoteAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notes = App.get(this).getNotes();
+
+        initRecycler();
+    }
+
+    private void initRecycler() {
+        recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new NoteAdapter();
+        adapter.setData(notes.getNotes());
+        adapter.setOnDeleteClickListener(this);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDeleteNote(Note note) {
+        notes.deleteNote(note);
+        adapter.setData(notes.getNotes());
+    }
+
+    @Override
+    public void onClickNote(Note note) {
+        Intent intent = new Intent(this, NoteActivity.class);
+        intent.putExtra(NoteActivity.NOTE_EXTRA_KEY, note);
+        startActivityForResult(intent, NOTE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NOTE_REQUEST_CODE && resultCode == RESULT_OK) {
+            adapter.setData(notes.getNotes());
+        }
     }
 }
---
+++ app/src/main/java/me/bkkn/ui/NoteAdapter.java
@@ -0,0 +1,48 @@
+package me.bkkn.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.bkkn.domain.entity.Note;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteViewHolder> {
+    private List<Note> data = new ArrayList<>();
+    private OnNoteListener onNoteListener;
+
+    public void setOnDeleteClickListener(OnNoteListener onNoteListener) {
+        this.onNoteListener = onNoteListener;
+    }
+
+    public void setData(List<Note> notes) {
+        data = notes;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        return new NoteViewHolder(inflater, parent, onNoteListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.bind(getItem(position));
+    }
+
+    private Note getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+}
