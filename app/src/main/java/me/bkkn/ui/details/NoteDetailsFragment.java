1be652286e6e32863a0b82c867f75664e7f2c88b
bkkn <bakaikin@yandex.ru>
2022-01-21 23:10:29 +0300

    some updates

--- app/src/main/java/me/bkkn/ui/details/NoteDetailsFragment.java
+++ app/src/main/java/me/bkkn/ui/details/NoteDetailsFragment.java
@@ -1,7 +1,6 @@
 package me.bkkn.ui.details;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
