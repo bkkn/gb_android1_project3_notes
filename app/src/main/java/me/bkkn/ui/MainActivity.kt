bf53683524bc1ae394f328beff8e24219451586e
bkkn_h <bakaikin@yandex.ru>
2022-08-18 01:32:41 +0300

    Kotlin support added, MainActivity.java -> MainActivity.kt

--- app/src/main/java/me/bkkn/ui/MainActivity.kt
+++ app/src/main/java/me/bkkn/ui/MainActivity.kt
@@ -1,89 +1,68 @@
-package me.bkkn.ui;
+package me.bkkn.ui
 
-import static me.bkkn.ui.NoteActivity.NOTE_EXTRA_KEY;
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import me.bkkn.App
+import me.bkkn.databinding.ActivityMainBinding
+import me.bkkn.domain.entity.Note
+import me.bkkn.domain.repository.Notes
+import me.bkkn.ui.list.NoteAdapter
+import me.bkkn.ui.list.NoteViewHolder.OnNoteListener
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var notes: Notes? = null
+    private var adapter: NoteAdapter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-import java.util.List;
-
-import me.bkkn.App;
-import me.bkkn.R;
-import me.bkkn.domain.entity.Note;
-import me.bkkn.domain.repository.Notes;
-import me.bkkn.ui.list.NoteAdapter;
-import me.bkkn.ui.list.NoteViewHolder;
-
-public class MainActivity extends AppCompatActivity {
-    private static final int NOTE_REQUEST_CODE = 42;
-
-    private Notes notes;
-
-    private RecyclerView recyclerView;
-    private NoteAdapter adapter;
-
-    private Button addNewNoteButton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        notes = App.get(this).getNotes();
-
-        initRecycler();
+        setContentView(binding.root)
+        notes = App.get(this).notes
+        initRecycler()
     }
 
-    private void initRecycler() {
-        recyclerView = findViewById(R.id.recycler_view);
-        addNewNoteButton = findViewById(R.id.add_note_button);
-
-        addNewNoteButton.setOnClickListener(v -> {
-            notes.addNewNote();
-            List<Note> list = notes.getNotes();
-            adapter.setData(list);
-            adapter.notifyItemInserted(list.size() - 1);
-            recyclerView.smoothScrollToPosition(list.size() - 1);
-        });
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        adapter = new NoteAdapter();
-        List<Note> list = notes.getNotes();
-        adapter.setData(list);
-        adapter.setOnDeleteClickListener(new NoteViewHolder.OnNoteListener() {
-            @Override
-            public void onDeleteNote(Note note) {
-                notes.deleteNote(note);
-                adapter.setData(notes.getNotes());
+    private fun initRecycler() {
+        binding.addNoteButton.setOnClickListener {
+            notes!!.addNewNote()
+            val list = notes!!.notes
+            adapter!!.setData(list)
+            adapter!!.notifyItemInserted(list.size - 1)
+            binding.recyclerView.smoothScrollToPosition(list.size - 1)
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = NoteAdapter()
+        val list = notes!!.notes
+        adapter!!.setData(list)
+        adapter!!.setOnDeleteClickListener(object : OnNoteListener {
+            override fun onDeleteNote(note: Note) {
+                notes!!.deleteNote(note)
+                adapter!!.setData(notes!!.notes)
             }
 
-            @Override
-            public void onClickNote(Note note) {
-                Intent intent = new Intent(MainActivity.this, NoteActivity.class);
-                intent.putExtra(NOTE_EXTRA_KEY, note);
-                startActivityForResult(intent, NOTE_REQUEST_CODE);
+            override fun onClickNote(note: Note) {
+                val intent = Intent(this@MainActivity, NoteActivity::class.java)
+                intent.putExtra(NoteActivity.NOTE_EXTRA_KEY, note)
+                startActivityForResult(intent, NOTE_REQUEST_CODE)
             }
-        });
-
-        recyclerView.setAdapter(adapter);
+        })
+        binding.recyclerView.setAdapter(adapter)
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == NOTE_REQUEST_CODE && resultCode == RESULT_OK) {
-            adapter.setData(notes.getNotes());
+            adapter!!.setData(notes!!.notes)
             //Note note = data.getParcelableExtra(NOTE_EXTRA_KEY);
             //int idx = App.get(this).getNotes().index(note);
-            adapter.notifyDataSetChanged();
+            adapter!!.notifyDataSetChanged()
         }
+    }
+
+    companion object {
+        private const val NOTE_REQUEST_CODE = 42
     }
 }
